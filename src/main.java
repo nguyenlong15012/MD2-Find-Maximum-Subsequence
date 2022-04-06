c42d6374232ec9ca13313992167eb42b1a2720ad
Nguyen Long <nguyenlong232253@gmail.com>
2022-04-06 11:03:34 +0700

    nopbaitap

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập chuối: ");
+        String str = sc.nextLine();
+
+        LinkedList<Character> max = new LinkedList<>();
+        for (int i = 0; i<str.length(); i++){
+            LinkedList<Character> list = new LinkedList<>();
+
+            list.add(str.charAt(i));
+            for (int j = i+1; j < str.length(); j++) {
+                if (str.charAt(j) > list.getLast()){
+                    list.add(str.charAt(j));
+                }
+            }
+            if (list.size()>max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+
+        for (Character ch : max){
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+}
